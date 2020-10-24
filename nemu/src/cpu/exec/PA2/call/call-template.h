73d8c5412e46242510b9414bbf96075d48b0da22
tracer-ics2016 <tracer@njuics.org>
2020-10-24 14:56:28 +0000

    >  compile NEMU 151220000 simpleedu Linux simpleedu 4.15.0-122-generic #124-Ubuntu SMP Thu Oct 15 13:03:05 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  14:56:28 up  1:29,  1 user,  load average: 0.85, 0.88, 0.89 85c47597ffe1895c90251b4ae4a5c3632d51187c

--- nemu/src/cpu/exec/PA2/call/call-template.h
+++ nemu/src/cpu/exec/PA2/call/call-template.h
@@ -1,15 +1,16 @@
 #include "cpu/exec/template-start.h"
 
-#define  instr call
+#define instr call
 
-make_helper (concat(call_i_, SUFFIX))
+make_helper(concat(call_i_, SUFFIX))
 {
-    int len =  concat(decode_i_, SUFFIX)(eip+1);
+    int len = concat(decode_i_, SUFFIX)(eip + 1);
     len += 1;
-    cpu.esp -= 4;
-    swaddr_write(cpu.esp, 4, cpu.eip);
+    reg_l(R_ESP) -= DATA_BYTE;
+    swaddr_write(reg_l(R_ESP), 4, cpu.eip);
+    DATA_TYPE_S displacement = op_src->val;
     cpu.eip += op_src->val;
-    print_asm("call %x",cpu.eip+1+len);
+    print_asm("call %x", cpu.eip + 1 + len + displacement);
     return len;
 }
 
@@ -19,6 +20,5 @@ make_helper (concat(call_rm_, SUFFIX))
  printf("call_rm_ ");
 }
 */
-
 
 #include "cpu/exec/template-end.h"
