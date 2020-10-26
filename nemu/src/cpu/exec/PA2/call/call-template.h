cead023b03f71d4076f1798bd3c8c3e4bcb2f6e0
tracer-ics2016 <tracer@njuics.org>
2020-10-26 14:09:36 +0000

    >  compile NEMU 151220000 simpleedu Linux simpleedu 4.15.0-122-generic #124-Ubuntu SMP Thu Oct 15 13:03:05 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  14:09:36 up 2 days, 42 min,  1 user,  load average: 0.57, 0.71, 0.66 859d33f86e4086172f20ab214e249a4b5a12fd42

--- nemu/src/cpu/exec/PA2/call/call-template.h
+++ nemu/src/cpu/exec/PA2/call/call-template.h
@@ -7,7 +7,7 @@ make_helper(concat(call_i_, SUFFIX))
     int len = concat(decode_i_, SUFFIX)(eip + 1);
     len = len +1;
     reg_l(R_ESP) -= DATA_BYTE;
-    swaddr_write(reg_l(R_ESP), 4, cpu.eip +len -1 );
+    swaddr_write(reg_l(R_ESP), 4, cpu.eip +len  );
     DATA_TYPE_S displacement = op_src->val;
     cpu.eip  = cpu.eip+displacement;
     print_asm("call 0x%08x   ", cpu.eip);
