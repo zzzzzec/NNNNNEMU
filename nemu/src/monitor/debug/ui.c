f17ed2695c374c31fec56209c526747c6232a9b4
tracer-ics2016 <tracer@njuics.org>
2020-10-26 13:41:37 +0000

    >  compile NEMU 151220000 simpleedu Linux simpleedu 4.15.0-122-generic #124-Ubuntu SMP Thu Oct 15 13:03:05 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  13:41:37 up 2 days, 14 min,  1 user,  load average: 1.08, 0.54, 0.39 61094192125d64424350d5f8f2764f6767811b91

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -75,6 +75,7 @@ static int cmd_x(char *args) {
 				}
 
 				printf("0x%08x ", swaddr_read(addr, 4));
+				printf("  %x ",swaddr_read(addr,1));
 				addr += 4;
 				if(i % 4 == 3) {
 					printf("\n");
