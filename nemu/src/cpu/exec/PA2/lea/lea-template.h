a1e50db19b0612c21656abb882647c0f158185d2
tracer-ics2016 <tracer@njuics.org>
2020-10-28 15:19:45 +0000

    >  compile NEMU 151220000 simpleedu Linux simpleedu 4.15.0-122-generic #124-Ubuntu SMP Thu Oct 15 13:03:05 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  15:19:45 up 4 days,  1:52,  1 user,  load average: 1.01, 0.84, 0.81 e0dec51f179a42a98ec074c1cff7a2c2158f7673

--- nemu/src/cpu/exec/PA2/lea/lea-template.h
+++ nemu/src/cpu/exec/PA2/lea/lea-template.h
@@ -5,13 +5,7 @@
 static void do_execute(){
     DATA_TYPE_S  addr = op_dest->val;
     int reg = op_src ->reg;
-    print_asm("%d %d %d",addr,reg,DATA_BYTE);
-
-
-
-
-
-
+    cpu.gpr[reg]._32 = addr;
 }
 
 make_instr_helper (r2rm);
