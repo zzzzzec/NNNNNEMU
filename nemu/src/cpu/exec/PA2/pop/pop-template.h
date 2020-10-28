f2b5634d0c549240a47639efc24638e31e409b9c
tracer-ics2016 <tracer@njuics.org>
2020-10-28 08:49:34 +0000

    >  compile NEMU 151220000 simpleedu Linux simpleedu 4.15.0-122-generic #124-Ubuntu SMP Thu Oct 15 13:03:05 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  08:49:34 up 3 days, 19:22,  1 user,  load average: 1.00, 1.07, 1.01 8ebf3cdf6bd3d3b6b3b8c08885dfe2ffd4afbd45

--- nemu/src/cpu/exec/PA2/pop/pop-template.h
+++ nemu/src/cpu/exec/PA2/pop/pop-template.h
@@ -3,40 +3,10 @@
 #define instr pop
 
 static void do_execute() {
-int reg = ops_decoded.opcode & 0x7;
-DATA_TYPE data=swaddr_read(cpu.esp,DATA_BYTE);
-cpu.gpr[check_reg_index(reg)]._32 = data; 
-cpu.esp = cpu.esp +4;
-switch (reg)
-   {
-   case  0:
-       print_asm("push %%eax ");
-       break; 
-      case  1:
-       print_asm("push %%ecx ");
-       break; 
-          case  2:
-       print_asm("push %%edx ");
-       break; 
-          case  3:
-       print_asm("push %%ebx ");
-       break; 
-          case  4:
-       print_asm("push %%esp ");
-       break; 
-          case  5:
-       print_asm("push %%ebp ");
-       break; 
-          case  6:
-       print_asm("push %%esi ");
-       break; 
-          case  7:
-       print_asm("push %%edi ");
-       break; 
-   default:
-       break;
-   }
-
+           DATA_TYPE result = swaddr_read(cpu.esp , DATA_BYTE);
+            int reg = ops_decoded.opcode & 0x7;
+            cpu.gpr[reg]._32 = result;
+            REG(R_ESP)+=DATA_BYTE;
 }
 
 make_instr_helper(r);
