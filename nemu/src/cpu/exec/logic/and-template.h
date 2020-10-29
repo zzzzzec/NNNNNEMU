b444a44d8a8aa7bfcc742ffd738b97cfd930b46d
tracer-ics2016 <tracer@njuics.org>
2020-10-29 03:09:38 +0000

    >  compile NEMU 151220000 simpleedu Linux simpleedu 4.15.0-122-generic #124-Ubuntu SMP Thu Oct 15 13:03:05 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  03:09:38 up 4 days, 13:42,  1 user,  load average: 0.95, 1.29, 1.19 47ecc96f9dbc4145c1986a76e17baacf69664342

--- nemu/src/cpu/exec/logic/and-template.h
+++ nemu/src/cpu/exec/logic/and-template.h
@@ -7,7 +7,29 @@ static void do_execute () {
 	OPERAND_W(op_dest, result);
 
 	/* TODO: Update EFLAGS. */
-	panic("please implement me");
+	/*panic("please implement me");*/
+	cpu.FLAG.CF = 0;
+	cpu.FLAG.OF =0;
+/*COMMON_SF_FLAG  MODULE  ===== START*/
+int  len = (DATA_BYTE << 3)-1;
+ cpu.FLAG.SF = result >> len;
+ /*COMMON_SF_FLAG  MODULE  ===== END*/
+ 	/*COMMON_ZF_FLAG  MODULE  ===== START*/
+	 if (result == 0 )     cpu.FLAG.ZF =1;
+    else                           cpu.FLAG.ZF = 0;
+    /*COMMON_ZF_FLAG  MODULE  ===== END*/
+		/*COMMON_PF_FLAG  MODULE  ===== START*/
+	int count = 0;int i;             DATA_TYPE tem =result;
+    for (  i = 0; i <8; i++)
+    {
+        tem = tem >> i;
+       if((tem & 1)==1 )         count ++;
+       tem = result;
+    }
+    if (count%2==0)       cpu.FLAG.PF = 1;
+    else                                cpu.FLAG.PF = 0;
+/*COMMON_PF_FLAG  MODULE  ===== END*/
+
 
 	print_asm_template2();
 }
