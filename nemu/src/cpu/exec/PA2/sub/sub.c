3c69ad62fd3c425172317901362c4f04d0c800ea
tracer-ics2016 <tracer@njuics.org>
2020-10-31 23:31:20 +0800

    >  compile NEMU 151220000 zhangenci Linux zhangenci-VirtualBox 5.4.0-52-generic #57~18.04.1-Ubuntu SMP Thu Oct 15 14:04:49 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  23:31:20 up 33 min,  1 user,  load average: 0.53, 0.55, 0.49 e78e705026925380e8b095731a4618b71586573c

--- nemu/src/cpu/exec/PA2/sub/sub.c
+++ nemu/src/cpu/exec/PA2/sub/sub.c
@@ -12,7 +12,8 @@
 #include "sub-template.h"
 #undef DATA_BYTE
 
-make_helper_v(sub_si2rm);
-make_helper_v(sub_rm2r);
-make_helper_v(sub_i2a);
-make_helper_v(sub_r2rm );
+make_helper_v(sub_i2a)
+make_helper_v(sub_i2rm)
+make_helper_v(sub_r2rm)
+make_helper_v(sub_rm2r)
+make_helper_v(sub_si2rm)
