dc6af243329438ca73f328e9886ce986fc6e538e
tracer-ics2016 <tracer@njuics.org>
2020-10-31 21:40:34 +0800

    >  compile NEMU 151220000 zhangenci Linux zhangenci-VirtualBox 5.4.0-52-generic #57~18.04.1-Ubuntu SMP Thu Oct 15 14:04:49 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  21:40:34 up  8:17,  1 user,  load average: 0.34, 0.30, 0.18 438f722b93c7326b8f28be2ad185c95185d72bf

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -194,7 +194,7 @@ helper_fun _2byte_opcode_table [256] = {
 /* 0x78 */	inv, inv, inv, inv, 
 /* 0x7c */	inv, inv, inv, inv, 
 /* 0x80 */	inv, inv, inv, inv,
-/* 0x84 */	inv, inv, inv, inv,
+/* 0x84 */	inv, inv,jbe_i_v, inv,
 /* 0x88 */ inv, jne_i_v, inv, inv, 
 /* 0x8c */	jl_i_v, inv, inv, jg_i_v, 
 /* 0x90 */	inv, inv, inv, inv,
