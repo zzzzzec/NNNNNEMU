219d20d36a3b55c10a9167108d3a5c7f318e6af2
tracer-ics2016 <tracer@njuics.org>
2020-10-31 08:04:27 +0000

    >  compile NEMU 151220000 simpleedu Linux simpleedu 4.15.0-122-generic #124-Ubuntu SMP Thu Oct 15 13:03:05 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  08:04:27 up  6:42,  1 user,  load average: 1.37, 1.28, 0.98 877217a2eed136855444a51ba5bc9c5806b8399

--- nemu/src/cpu/exec/PA2/add/add-template.h
+++ nemu/src/cpu/exec/PA2/add/add-template.h
@@ -1,4 +1,41 @@
 #include "cpu/exec/template-start.h"
 
-#define instr call
+#define instr add
+
+static void do_execute() {
+	DATA_TYPE result = op_dest->val + op_src->val;
+    OPERAND_W(op_dest, result);
+	int len = (DATA_BYTE << 3) - 1;
+	int s1,s2;
+	cpu.FLAG.CF=(result < op_dest->val);
+	cpu.FLAG.SF=result >> len;
+	s1=op_dest->val>>len;
+	s2=op_src->val>>len;
+    	cpu.FLAG.OF=(s1 == s2 && s1 != cpu.FLAG.SF) ;
+		/*COMMON_ZF_FLAG  MODULE  ===== START*/
+	 if (result == 0 )     cpu.FLAG.ZF =1;
+    else                           cpu.FLAG.ZF = 0;
+    /*COMMON_ZF_FLAG  MODULE  ===== END*/
+	
+	/*COMMON_PF_FLAG  MODULE  ===== START*/
+	int count1 = 0;int i;             DATA_TYPE tem =result;
+    for (  i = 0; i <8; i++)
+    {
+        tem = tem >> i;
+       if((tem & 1)==1 )         count1 ++;
+       tem = result;
+    }
+    if (count1%2==0)       cpu.FLAG.PF = 1;
+    else                                cpu.FLAG.PF = 0;
+/*COMMON_PF_FLAG  MODULE  ===== END*/
+	print_asm_template2();
+}
+#if DATA_BYTE == 2 || DATA_BYTE == 4
+make_instr_helper(si2rm)
+#endif
+
+make_instr_helper(i2a)
+make_instr_helper(i2rm)
+make_instr_helper(r2rm)
+make_instr_helper(rm2r)
 #include "cpu/exec/template-end.h"
--- nemu/src/cpu/exec/PA2/add/add.c
+++ nemu/src/cpu/exec/PA2/add/add.c
@@ -11,3 +11,9 @@
 #define DATA_BYTE 4
 #include "add-template.h"
 #undef DATA_BYTE
+
+make_helper_v(add_i2a)
+make_helper_v(add_i2rm)
+make_helper_v(add_r2rm)
+make_helper_v(add_rm2r)
+make_helper_v(add_si2rm)
