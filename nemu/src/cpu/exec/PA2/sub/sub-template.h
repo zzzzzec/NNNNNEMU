9a4d6d1259dab03a84e72da2a492a7b5876263c5
tracer-ics2016 <tracer@njuics.org>
2020-10-31 23:22:33 +0800

    >  compile NEMU 151220000 zhangenci Linux zhangenci-VirtualBox 5.4.0-52-generic #57~18.04.1-Ubuntu SMP Thu Oct 15 14:04:49 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  23:22:33 up 24 min,  1 user,  load average: 0.48, 0.51, 0.46 f2157dcfd0d554a1a6eb9987742e20d5f09ea2e

--- nemu/src/cpu/exec/PA2/sub/sub-template.h
+++ nemu/src/cpu/exec/PA2/sub/sub-template.h
@@ -2,7 +2,7 @@
 
 #define instr sub
 static void do_execute(){
-     int  len = (DATA_BYTE << 3)-1;
+    int  len = (DATA_BYTE << 3)-1;
    DATA_TYPE result = op_dest->val - op_src->val;
    OPERAND_W(op_dest,result);
    /*set flag*/
@@ -49,9 +49,11 @@ else
 cpu.FLAG.SF = (cpu.FLAG.SF) ^ (cpu.FLAG.OF) ;
 print_asm_template2();
 }
-make_instr_helper(i2a);
-make_instr_helper(rm2r);
-make_instr_helper(r2rm);
+make_instr_helper(i2a)
+make_instr_helper(i2rm)
+make_instr_helper(r2rm)
+make_instr_helper(rm2r)
+
 #if   (DATA_BYTE == 2 || DATA_BYTE ==4)
 make_instr_helper(si2rm);
 #endif
