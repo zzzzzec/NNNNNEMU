2b1c755b7f07937da742efda831a0a45548b9c9e
tracer-ics2016 <tracer@njuics.org>
2020-10-31 22:22:17 +0800

    >  compile NEMU 151220000 zhangenci Linux zhangenci-VirtualBox 5.4.0-52-generic #57~18.04.1-Ubuntu SMP Thu Oct 15 14:04:49 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  22:22:17 up  8:58,  1 user,  load average: 0.43, 0.53, 0.45 4a51b31d7f70a1b3918857152449a5191c7d3611

--- nemu/src/cpu/exec/logic/xor-template.h
+++ nemu/src/cpu/exec/logic/xor-template.h
@@ -5,9 +5,28 @@
 static void do_execute () {
 	DATA_TYPE result = op_dest->val ^ op_src->val;
 	OPERAND_W(op_dest, result);
-
-	/* TODO: Update EFLAGS. */
-	panic("please implement me");
+	/*COMMON_ZF_FLAG  MODULE  ===== START*/
+	 if (result == 0 )     cpu.FLAG.ZF =1;
+    else                           cpu.FLAG.ZF = 0;
+    /*COMMON_ZF_FLAG  MODULE  ===== END*/
+	
+	/*COMMON_PF_FLAG  MODULE  ===== START*/
+	int count1 = 0;int i;             DATA_TYPE tem =result;
+    for (  i = 0; i <8; i++)
+    {
+        tem = tem >> i;
+       if((tem & 1)==1 )         count1 ++;
+       tem = result;
+    }
+    if (count1%2==0)       cpu.FLAG.PF = 1;
+    else                                cpu.FLAG.PF = 0;
+/*COMMON_PF_FLAG  MODULE  ===== END*/
+	/*COMMON_SF_FLAG  MODULE  ===== START*/
+int  len = (DATA_BYTE << 3)-1;
+ cpu.FLAG.SF = result >> len;
+ /*COMMON_SF_FLAG  MODULE  ===== END*/
+  cpu.FLAG.CF   =0;
+  cpu.FLAG.OF   =0;
 
 	print_asm_template2();
 }
