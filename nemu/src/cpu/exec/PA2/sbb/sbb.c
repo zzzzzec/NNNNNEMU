8d0109a6717e2fb551caac15459a6121c1156248
tracer-ics2016 <tracer@njuics.org>
2020-11-01 21:57:58 +0800

    >  compile NEMU 151220000 zhangenci Linux zhangenci-VirtualBox 5.4.0-52-generic #57~18.04.1-Ubuntu SMP Thu Oct 15 14:04:49 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  21:57:58 up  7:30,  1 user,  load average: 0.13, 0.35, 0.46 85e3e5974c619836a26d847ba6cc141be163328

--- nemu/src/cpu/exec/PA2/sbb/sbb.c
+++ nemu/src/cpu/exec/PA2/sbb/sbb.c
@@ -11,3 +11,10 @@
 #define DATA_BYTE 4
 #include "sbb-template.h"
 #undef DATA_BYTE
+
+
+make_helper_v(sbb_i2a)
+make_helper_v(sbb_i2rm)
+make_helper_v(sbb_r2rm)
+make_helper_v(sbb_rm2r)
+make_helper_v(sbb_si2rm)
