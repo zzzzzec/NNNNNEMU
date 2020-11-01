2ab8c56005df17cd2642c2f12ddd203c225dfc67
tracer-ics2016 <tracer@njuics.org>
2020-11-01 21:44:20 +0800

    >  compile NEMU 151220000 zhangenci Linux zhangenci-VirtualBox 5.4.0-52-generic #57~18.04.1-Ubuntu SMP Thu Oct 15 14:04:49 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  21:44:20 up  7:17,  1 user,  load average: 0.20, 0.39, 0.46 684efeda61b12733f5dd77c24d080ceafc5cdabc

---
+++ nemu/src/cpu/exec/PA2/jcc/jae-template.h
@@ -0,0 +1,14 @@
+#include "cpu/exec/template-start.h"
+
+#define instr jae
+
+
+static void do_execute() {
+	DATA_TYPE_S displacement = op_src->val;
+	print_asm("jae %x",cpu.eip + 1 + DATA_BYTE + displacement);
+	if (cpu.FLAG.CF == 0)cpu.eip +=displacement;
+}
+make_instr_helper(i)
+
+
+#include "cpu/exec/template-end.h"
---
+++ nemu/src/cpu/exec/PA2/jcc/jcc.h
@@ -0,0 +1,16 @@
+#include <ja.h>
+#include<jae.h>
+#include<jb.h>
+#include<jbe.h>
+#include<je.h>
+#include<jg.h>
+#include<jge.h>
+#include<jl.h>
+#include<jle.h>
+#include<jne.h>
+#include<jno.h>
+#include<jnp.h>
+#include<jns.h>
+#include<jo.h>
+#include<jp.h>
+#include<js.h>
