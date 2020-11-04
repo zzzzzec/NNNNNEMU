f839f5b1ec37423a58c506c60a3dbf9ddcb0dc59
tracer-ics2016 <tracer@njuics.org>
2020-11-04 16:30:58 +0800

    >  compile NEMU 151220000 zhangenci Linux zhangenci-VirtualBox 5.4.0-52-generic #57~18.04.1-Ubuntu SMP Thu Oct 15 14:04:49 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:30:58 up  2:00,  1 user,  load average: 0.37, 0.38, 0.57 92e8874626fbd79aec28fb89b34849b6df368b6

--- nemu/src/cpu/exec/PA2/call/call-template.h
+++ nemu/src/cpu/exec/PA2/call/call-template.h
@@ -23,7 +23,7 @@ static void do_execute() {
     cpu.eip  = cpu.eip+displacement;
     int addr;
     addr = cpu.eip + displacement;
-    print_asm("call %d       %d      %d",addr,DATA_BYTE,displacement);
+    print_asm("call %0xd       %0xd      %0xd",addr,DATA_BYTE,displacement);
 }
 
 make_instr_helper(i);
